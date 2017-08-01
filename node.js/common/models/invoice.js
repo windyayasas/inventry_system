9e99ae9636a397b7d0a0dede7068483a303b6b0e
windyayasas <windyayasaspathirana9@gmail.com>
2017-08-01 07:21:38 +0530

    added new service

--- node.js/common/models/invoice.js
+++ node.js/common/models/invoice.js
@@ -1,5 +1,41 @@
 'use strict';
 
-module.exports = function(Invoice) {
+module.exports = function (Invoice) {
+
+
+    Invoice.addInvoice = function (paymentDueDate, currency, paidAmount, callback) {
+
+        paymentDueDate = new Date(paymentDueDate);
+        var creatObj = {
+            issueDate: new Date(),
+            paymentDue: paymentDueDate,
+            currency: currency,
+            total: paidAmount,
+            paidAmount: paidAmount,
+            lastUpdate: new Date()
+        }
+        Invoice.create(creatObj, function (err, result) {
+            if (err)
+            {
+                callback(err, null);
+            }
+            else {
+                callback(err, "success");
+            }
+        });
+    }
+    Invoice.remoteMethod(
+            'addInvoice',
+            {
+                description: 'Add invoice',
+                accepts: [
+                    {arg: 'paymentDueDate', type: 'string', required: true},
+                    {arg: 'currency', type: 'string', required: true},
+                    {arg: 'paidAmount', type: 'number', required: true}
+                ],
+                returns: {type: 'string', arg: 'result'},
+                http: {verb: 'post'}
+            }
+    );
 
 };
