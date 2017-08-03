65d55aaca424f4a6f526ddf897940ad52cc3397e
chathuri123 <chathuridewanarayana@gmail.com>
2017-08-03 19:32:03 +0530

    done company.js and product.js

--- node.js/common/models/company.js
+++ node.js/common/models/company.js
@@ -2,4 +2,71 @@
 
 module.exports = function(Company) {
 
+	Company.addCompany = function (name, type, taxNumber, phoneNumber, faxNumber, website, email, description, tag, defaultPriceList, defaultDiscount, defaultPaymentMethod, defaultStockLocation, assignedTo, addressName, country, addressLine01, addressLine02, city, state, zipCode, callback) {
+
+        var creatObj = {
+            name: name,
+            type: type,
+            taxNumber: taxNumber,
+            phoneNumber: phoneNumber,
+            faxNumber: faxNumber,
+            website: website,
+            email: email,
+            description: description,
+            tag: tag,
+            defaultPriceList: defaultPriceList,
+            defaultDiscount: defaultDiscount,
+            defaultPaymentMethod: defaultPaymentMethod,
+            defaultStockLocation: defaultStockLocation,
+            assignedTo: assignedTo,
+            addressName: addressName,
+            country: country,
+            addressLine01: addressLine01,
+            addressLine02: addressLine02,
+            city: city,
+            state: state,
+            zipCode: zipCode
+        }
+        Company.create(creatObj, function (err, result) {
+            if (err)
+            {
+                callback(err, null);
+            }
+            else {
+                callback(err, "success");
+            }
+        });
+    }
+    Company.remoteMethod(
+            'addCompany',
+            {
+                description: 'Add company',
+                accepts: [
+                    {arg: 'name', type: 'string', required: true},
+                    {arg: 'type', type: 'string', required: true},
+                    {arg: 'taxNumber', type: 'number', required: true},
+                    {arg: 'phoneNumber', type: 'number', required: true},
+                    {arg: 'faxNumber', type: 'number'},
+                    {arg: 'website', type: 'string'}
+                    {arg: 'email', type: 'string', required: true},
+                    {arg: 'description', type: 'string', required: true},
+                    {arg: 'tag', type: 'string', required: true},
+                    {arg: 'defaultPriceList', type: 'string', required: true},
+                    {arg: 'defaultDiscount', type: 'number', required: true},
+                    {arg: 'defaultPaymentMethod', type: 'string', required: true},
+                    {arg: 'defaultStockLocation', type: 'string'},
+                    {arg: 'assignedTo', type: 'string', required: true},
+                    {arg: 'addressName', type: 'string', required: true},
+                    {arg: 'country', type: 'string', required: true},
+                    {arg: 'addressLine01', type: 'string', required: true},
+                    {arg: 'addressLine02', type: 'string', required: true},
+                    {arg: 'city', type: 'string', required: true},
+                    {arg: 'state', type: 'string', required: true},
+                    {arg: 'zipCode', type: 'number', required: true}
+                ],
+                returns: {type: 'string', arg: 'result'},
+                http: {verb: 'post'}
+            }
+    );
+
 };
--- node.js/common/models/product.js
+++ node.js/common/models/product.js
@@ -2,4 +2,60 @@
 
 module.exports = function(Product) {
 
+	Product.addProduct = function (name, type, brand, description, tag, initialCostPrice, buyPrice, wholesalePrice, retailPrice, initialStockLocation, initialStockOnHand, weight, batchTracked, callback) {
+
+        var creatObj = {
+            name: name,
+            type: type,
+            brand: brand,
+            description: description,
+            tag: tag,
+            initialCostPrice: initialCostPrice,
+            buyPrice: buyPrice,
+            wholesalePrice: wholesalePrice,
+            retailPrice: retailPrice,
+            initialStockLocation: initialStockLocation,
+            initialStockOnHand: initialStockOnHand, 
+            weight: weight, 
+            batchTracked: batchTracked,
+            
+        }
+        Product.create(creatObj, function (err, result) {
+            if (err)
+            {
+                callback(err, null);
+            }
+            else {
+                callback(err, "success");
+            }
+        });
+    }
+
+    Product.remoteMethod(
+            'addProduct',
+            {
+                description: 'Add Product',
+                accepts: [
+                    {arg: 'name', type: 'string', required: true},
+                    {arg: 'type', type: 'string', required: true},
+                    {arg: 'brand', type: 'string', required: true},
+                    {arg: 'description', type: 'string', required: true},
+                    {arg: 'tag', type: 'string'},
+                    {arg: 'initialCostPrice', type: 'number', required: true},
+                    {arg: 'buyPrice', type: 'number', required: true},
+                    {arg: 'wholesalePrice', type: 'number', required: true},
+                    {arg: 'retailPrice', type: 'number', required: true},
+                    {arg: 'initialStockLocation', type: 'string', required: true},
+                    {arg: 'initialStockOnHand', type: 'number', required: true},
+                    {arg: 'weight', type: 'number'},
+                    {arg: 'batchTracked', type: 'boolean', required: true}
+                    
+                ],
+                returns: {type: 'string', arg: 'result'},
+                http: {verb: 'post'}
+            }
+    );
+
+
+
 };
